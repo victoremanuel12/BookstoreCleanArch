3d41c0ec696806601436acbf35b871b8eef29f27
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-11-01 14:43:27 -0300

    adição ao propram do  IExceptionHandler

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,12 +1,11 @@
 using Api.GlobalHandling;
-using Api.Middleware;
 using Infra.IoC;
-using Microsoft.AspNetCore.Http;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddExceptionHandler<TimeOutExceptionHandler>();
 builder.Services.AddExceptionHandler<GlobalExceptionHandling>();
 builder.Services.AddProblemDetails();
 var app = builder.Build();
