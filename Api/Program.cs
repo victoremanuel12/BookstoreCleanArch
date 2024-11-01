d2b6b497dc74c14a25956d71a1b906855ee6726f
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-11-01 13:44:01 -0300

    finalização do serviço de medleare

--- Api/Program.cs
+++ Api/Program.cs
@@ -17,7 +17,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseExceptionHandler();
-// app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
